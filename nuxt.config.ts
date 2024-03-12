ada92e5c9629fd3fb530b5d6ebeb575a0254bcfa
Hussain Panahy <144150970+HP8585@users.noreply.github.com>
2024-03-12 17:52:58 +0330

    fifth commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
 
   runtimeConfig:{
     public:{
-      BASE_URL : "http://localhost:5000"
+      BASE_URL : "https://speedysphere-backend.vercel.app"
     }
   }
 })
